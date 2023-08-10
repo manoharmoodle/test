6079c05c04cfba7d4724e6e8746274c41ebf4232
Your Name <yislms@173.167.178.68.host.secureserver.net>
2023-08-10 08:52:42 +0000

    test

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'anaflix_main';
+$CFG->dbuser    = 'yatharthd';
+$CFG->dbpass    = 'Ya@8980@6849';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'https://yislms.com/anaflix';
+// $CFG->dataroot  = '/home/yislms/moodledata_anaflix';
+$CFG->dataroot  = '/home/yislms/anaflixdata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
